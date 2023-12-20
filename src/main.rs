b6b5cb5f2e6222d3167d274298e622e6965041f7
Square-Face <sq8.square.face@gmail.com>
2023-12-20 10:57:07 +0100

    clap setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Command;
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Command::new("PrusaSlicer Configuration Wizard")
+        .author("Linus Michelsson(Innovatoriet)")
+        .version("0.0.1-alpha1")
+        .about("Command line interface for managing prusa slicer configurations using git repositories")
+        .subcommand(
+            Command::new("init")
+        )
+        .subcommand(
+            Command::new("update")
+        );
+
+    let matches = cli.get_matches();
+    
 }
