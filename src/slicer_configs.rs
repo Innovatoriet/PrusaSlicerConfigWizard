2871455fa758421a4ae8f91f74f5e540cbf42afd
Square-Face <sq8.square.face@gmail.com>
2023-12-29 22:28:09 +0100

    Added ini formater

--- src/slicer_configs.rs
+++ src/slicer_configs.rs
@@ -1,8 +1,6 @@
-
 /// Handles parsing and writing individual PrusaSlicer configuration files
 use ini_core::{Item, Parser};
 use std::fs::read_to_string;
-
 
 /// Represents a PrusaSlicer configuration property within a section
 #[derive(Debug, Clone)]
@@ -112,7 +110,100 @@ impl File {
         Ok(file)
     }
 
+    /// Add a property to a string formatted as follows
+    /// `<key> = <value>\n`
+    /// Where key and value are strings.
+    /// value defaults to "" if it is None on the given property
+    ///
+    /// # Arguments
+    /// * `str` - String to add property to
+    /// * `prop` - Property to add
+    ///
+    /// # Example
+    /// ```rust
+    /// let mut str = String::new();
+    ///
+    /// add_property(&mut str,
+    ///     &Property {
+    ///         key: "foo".to_string(),
+    ///         value: Some("boo".to_string())
+    ///     });
+    ///
+    /// assert_eq!(str, "foo = boo\n");
+    /// ```
+    fn add_property(str: &mut String, prop: &Property) {
+        // convert String value to str or default to ""
+        let value = match &prop.value {
+            Some(value) => value.as_str(),
+            None => "",
+        };
+
+        // Format using '<key> = <value>'
+        let formated = format!("{} = {}\n", prop.key, value);
+        str.push_str(&formated);
+    }
+
+    /// Add a section to a string formatted as follows
+    /// `[<name>]\n`
+    /// Where name is a string
+    /// 
+    /// # Arguments
+    /// * `str` - String to add section to
+    /// * `section` - Section to add
+    ///
+    /// # Example
+    /// ```rust
+    /// let mut str = String::new();
+    /// 
+    /// add_section(&mut str,
+    ///     &Section {
+    ///         name: "foo".to_string(),
+    ///         properties: Vec::new()
+    ///     });
+    ///
+    /// assert_eq!(str, "[foo]\n");
+    /// ```
+    fn add_section(str: &mut String, section: &Section) {
+        // Format using '[<name>]'
+        let formated = format!("\n[{}]\n", section.name);
+
+        str.push_str(&formated);
+
+        // add section properties
+        for p in section.properties.iter() {
+            Self::add_property(str, &p);
+        }
+    }
+
+    /// Format a PrusaSlicer configuration file
+    /// 
+    /// # Arguments
+    /// * `out` - String to append formating to
+    ///
+    /// # Example
+    /// ```rust
+    /// let mut str = String::new();
+    /// 
+    /// let input = File {
+    ///     path: "foo.ini".to_string(),
+    ///     properties: Vec::new(),
+    ///     sections: Vec::new()
+    /// };
+    /// 
+    /// input.format(&mut str);
+    /// 
+    /// assert_eq!(str, "");
+    /// ```
+      
     pub fn format(&self, out: &mut String) {
-        todo!();
+        // add global properties
+        for p in self.properties.iter() {
+            Self::add_property(out, &p);
+        }
+
+        // add sections
+        for s in self.sections.iter() {
+            Self::add_section(out, &s)
+        }
     }
 }
