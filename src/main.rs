2e9ea907a87a504c9a23c9e14adca753663ada64
Square-Face <sq8.square.face@gmail.com>
2023-12-30 17:26:20 +0100

    Implemented functions for checking if a printer exists and has the required nozzle sizes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,44 @@
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string};
 
 mod interface;
 mod slicer_configs;
 
-
 const PATH: &str = "";
+
+// Make sure the given given printer exists and it has the given nozzle
+fn has_printer_and_nozzle<'a>(
+    printers: &mut HashMap<&'a str, Option<Vec<&'a str>>>,
+    name: &'a str,
+    nozzle: &'a str,
+) {
+    // Check if the printer exists, if it doesn't add it with the given nozzle and return
+    let printer = printers.entry(name).or_insert(None);
+
+    // check if the printer has nozzles
+    if let Some(nozzles) = printer {
+        // Check if it has the given nozzle, if it doesn't add it and return
+        if !nozzles.contains(&nozzle) {
+            nozzles.push(nozzle);
+        }
+        return;
+    }
+
+    // If the printer has no nozzles, add the given nozzle
+    printers.insert(name, Some(vec![nozzle]));
+}
 
 fn main() {
     let contents = read_to_string(PATH).expect("Something went wrong reading the file");
 
-    let config = slicer_configs::Config::parse(&contents).expect("Failed to parse config");
+    let config = slicer_configs::ConfigFile::parse(&contents).expect("Failed to parse config");
+    let mut map = config.to_map();
+    let prusa_vendor = map
+        .sections
+        .entry("vendor:PrusaResearch")
+        .or_insert(HashMap::new());
 
-    let mut out = String::new();
-    config.format(&mut out);
-    println!("{}", out);
+    has_printer_and_nozzle(prusa_vendor, "model:MK4IS", "0.2");
+    has_printer_and_nozzle(prusa_vendor, "model:MK3S", "0.2");
+
+    dbg!(map.to_file());
 }
